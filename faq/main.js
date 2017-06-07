fb1dbb56d024a53a2c092abc796f211cba18d117
Rachel Buckner <rachelbuckner@gmail.com>
2017-06-06 20:52:41 -0400

    Add files via upload

---
+++ faq/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $("#panel").hide();
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+
+    $("#panel1").hide();
+    $("#flip1").click(function(){
+        $("#panel1").slideToggle("slow");
+    });
+
+    $("#panel2").hide();
+    $("#flip2").click(function(){
+        $("#panel2").slideToggle("slow");
+    });
+
+    $("#panel3").hide();
+    $("#flip3").click(function(){
+        $("#panel3").slideToggle("slow");
+    });
+
+    $("#panel4").hide();
+    $("#flip4").click(function(){
+        $("#panel4").slideToggle("slow");
+    });
+});
